8593bdd46c66f1d5dcb5fa8b3846fc7cdd2418ce
Anit Shrestha Manandhar <codeanit@gmail.com>
2021-12-10 19:05:27 +0545

    Load all companies before unfollowing

---
+++ unfollow-companies.js
@@ -0,0 +1,37 @@
+/**
+
+Unfollow companies https://www.linkedin.com/feed/following/?filterType=company
+
+Copy paste the code in the `console`.
+
+Recommendation API: https://www.linkedin.com/voyager/api/feed/richRecommendedEntities?count=20&q=unfollowHubRecommendations&start=240&type=COMPANY
+
+**/
+
+function sleep(milliseconds) {  
+  return new Promise(resolve => setTimeout(resolve, milliseconds));  
+}
+
+async function loadCompany() {  
+  for (let i = 1; i <=100 ; i++) {          
+    await sleep(2000);  
+    window.scrollBy(0, 10000);
+  }  
+}
+
+function deleteCompany() {
+  var main = document.getElementById("main").getElementsByTagName("ul"); 
+  var liArray = main[1].getElementsByTagName("li");
+
+  for(var i=0; i < 1,374;i++) {
+
+    var button = liArray[i].getElementsByTagName('button');
+
+    if (button[0].innerText == "Following") { 
+      button[0].click();
+    }
+  }
+}
+
+loadCompany();
+deleteCompany();
