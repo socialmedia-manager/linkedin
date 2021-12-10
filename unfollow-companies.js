0dbdfd658691193a7f8b0fbc2af0a330331b8f7b
Anit Shrestha Manandhar <codeanit@gmail.com>
2021-12-10 19:45:03 +0545

    Add window reload
    After one unfollow cycle is complete, reload the browser so that the loop would not go through unfollowed ones.

--- unfollow-companies.js
+++ unfollow-companies.js
@@ -6,32 +6,35 @@ Copy paste the code in the `console`.
 
 Recommendation API: https://www.linkedin.com/voyager/api/feed/richRecommendedEntities?count=20&q=unfollowHubRecommendations&start=240&type=COMPANY
 
+First run load company and then delete company.
+
 **/
 
-function sleep(milliseconds) {  
-  return new Promise(resolve => setTimeout(resolve, milliseconds));  
+function sleep(milliseconds) {
+  return new Promise(resolve => setTimeout(resolve, milliseconds));
 }
 
-async function loadCompany() {  
-  for (let i = 1; i <=100 ; i++) {          
-    await sleep(2000);  
+async function loadCompany() {
+  for (let i = 1; i <= 100; i++) {
+    await sleep(2000);
     window.scrollBy(0, 10000);
-  }  
-}
-
-function deleteCompany() {
-  var main = document.getElementById("main").getElementsByTagName("ul"); 
-  var liArray = main[1].getElementsByTagName("li");
-
-  for(var i=0; i < 1,374;i++) {
-
-    var button = liArray[i].getElementsByTagName('button');
-
-    if (button[0].innerText == "Following") { 
-      button[0].click();
-    }
   }
 }
 
-loadCompany();
-deleteCompany();
+function deleteCompany() {
+  var main = document.getElementById("main").getElementsByTagName("ul");
+  var liArray = main[1].getElementsByTagName("li");
+
+  for (var i = 0; i < 1, 374; i++) {
+
+    var button = liArray[i].getElementsByTagName('button');
+
+    if (button[0].innerText == "Following") {
+      button[0].click();
+    }
+  }
+  window.location.reload();
+}
+
+// loadCompany();
+// deleteCompany();
